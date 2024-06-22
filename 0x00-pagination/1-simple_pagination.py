8254a420d389ec7bd66bcc4f82f45b3570e487fd
majidied <majidimajidi2003@gmail.com>
2024-06-22 03:04:26 +0100

    chore: Add pagination helper function and test script

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+import csv
+import math
+import os
+from typing import Tuple
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """
+    Server class to paginate
+    """
+
+    DATA_FILE = os.path.join(os.path.dirname(__file__), "Popular_Baby_Names.csv")
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters.
+        """
+        return index_range(page, page_size)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[str]:
+        """
+        Get page
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = self.index_range(page, page_size)
+        return self.dataset()[start:end]
