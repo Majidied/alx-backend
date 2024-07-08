728388d0f3ac4f331e8067d6e705f178cbed6109
majidied <majidimajidi2003@gmail.com>
2024-07-08 18:10:48 +0100

    chore: Refactor MRUCache implementation and add test script

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+""" MRU Caching """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Most Recently Used (MRU) Cache implementation.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: The key of the item.
+            item: The item to be added.
+
+        Returns:
+            None
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.order.pop()
+                del self.cache_data[discard]
+                print("DISCARD:", discard)
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """
+        Get an item from the cache by key.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The item associated with the key, or None if the key is not found.
+        """
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
