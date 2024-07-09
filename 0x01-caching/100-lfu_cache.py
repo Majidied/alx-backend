7940b5b4fb63bd3a7283f62d80d63655d234c93e
majidied <majidimajidi2003@gmail.com>
2024-07-09 02:06:10 +0100

    feat: Refactor LFUCache implementation to handle cache eviction when reaching maximum capacity

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,13 +4,32 @@ from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    
+    """
+    LFUCache class represents a Least Frequently Used (LFU) cache 
+    implementation.
+    It inherits from the BaseCaching class.
+
+    Attributes:
+        order (dict): A dictionary to keep track of the order of key usage.
+        lfu (None): A placeholder attribute, not used in this implementation.
+    """
+
     def __init__(self):
         super().__init__()
         self.order = {}
         self.lfu = None
     
     def put(self, key, item):
+        """
+        Adds an item to the cache.
+
+        Args:
+            key: The key of the item.
+            item: The item to be added.
+
+        Returns:
+            None
+        """
         if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
                 min_key = min(self.order, key=self.order.get)
@@ -22,9 +41,16 @@ class LFUCache(BaseCaching):
             self.order[key] = 0
     
     def get(self, key):
+        """
+        Retrieves an item from the cache.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The item associated with the key, or None if the key is not found.
+        """
         if key in self.cache_data:
             self.order[key] += 1
             return self.cache_data[key]
         return None
-
-
