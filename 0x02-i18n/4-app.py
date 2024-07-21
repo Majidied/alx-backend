ff69a5747789fe66ec2b8d8d3fa589672cef6b30
majidied <majidimajidi2003@gmail.com>
2024-07-21 08:34:40 +0100

    chore: Update Flask app configuration to use a separate Config class and add internationalization support

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Internationalization """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+    def __init__(self):
+        """ Constructor """
+        pass
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+def get_locale() -> str:
+    """ Get locale
+    Return: Best match language code for user
+    """
+    local = request.args.get('locale')
+    if local and local in app.config['LANGUAGES']:
+        print(local)
+        return local
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 0-index.html
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    babel.init_app(app, locale_selector=get_locale)
+    app.run(host="0.0.0.0", port="5000")
