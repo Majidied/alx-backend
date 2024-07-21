acee1593d8afde4e536e6651d80ba6a81cf6b198
majidied <majidimajidi2003@gmail.com>
2024-07-21 07:40:26 +0100

    chore: Update Flask app configuration to use a separate Config class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,16 +4,16 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-
 class Config:
     """ Config class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
+
     def __init__(self):
         """ Constructor """
         pass
+
 
 app = Flask(__name__)
 babel = Babel(app)
