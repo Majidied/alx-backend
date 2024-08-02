9a1a48aaa15a545cb4795c010a6bfa3a137f7c1c
majidied <majidimajidi2003@gmail.com>
2024-08-02 11:50:54 +0100

    Refactor Redis client initialization and error handling

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -8,7 +8,6 @@ redisClient.on('error', (error) => {
   console.log(`Redis client not connected to server: ${error.message}`);
   redisClient.quit();
 });
-redisClient.on('connect', () => console.log('Redis client connected to the server'));
 
 console.log(redisClient.connected);
 /**
@@ -22,15 +21,24 @@ function setNewSchool(schoolName, value) {
 
 /**
  * Gets and display the value associated with given key
- * in redis store.
+ * in redis store - asynchronously.
  * @param {string} schoolName - key to search in redis
  */
-function displaySchoolValue(schoolName) {
-    getAsync(schoolName).then((value) => {
-        console.log(value);
-    });
+async function displaySchoolValue(schoolName) {
+  const value = await getAsync(schoolName);
+  if (value) console.log(value);
 }
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+/**
+ * Entry point
+ */
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+redisClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+  main();
+});
