8f92b312318a34fece3c74b0e4fc1b8b48aefb55
majidied <majidimajidi2003@gmail.com>
2024-08-02 12:39:47 +0100

    Refactor Redis client initialization and error handling

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import { print, createClient } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('error', (error) => {
+  console.log(`Redis client not connected to server: ${error.message}`);
+  redisClient.quit();
+});
+
+redisClient.on('connect', () => console.log('Redis client connected to the server'));
+
+redisClient.hset('HolbertonSchools', 'Portland', '50', print);
+redisClient.hset('HolbertonSchools', 'Seattle', '80', print);
+redisClient.hset('HolbertonSchools', 'New York', '20', print);
+redisClient.hset('HolbertonSchools', 'Bogota', '20', print);
+redisClient.hset('HolbertonSchools', 'Cali', '40', print);
+redisClient.hset('HolbertonSchools', 'Paris', '2', print);
+
+
+redisClient.hgetall('HolbertonSchools', (error, object) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log(object);
+});
